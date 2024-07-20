9ba6533aac15fb3f173d70ef0282d806a21a2cfc
toto <antoniolabate19@gmail.com>
2024-07-20 11:03:45 +0200

    general configuration and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdsListComponent } from './ads-list/ads-list.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', pathMatch: 'full', component: HomeComponent },
+    { path: 'ads', pathMatch: 'full', component: AdsListComponent,
+      // canActivate: [authGuard]
+    },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
+
