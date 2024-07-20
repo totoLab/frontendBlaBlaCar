38faa4ea22698f0da33975be8000687574c7160e
toto <antoniolabate19@gmail.com>
2024-07-20 19:59:41 +0200

    added user module to app and configured paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { AdsListComponent } from './ads-list/ads-list.component';
 import { AdsListPageComponent } from './ads-list-page/ads-list-page.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, AdsListPageComponent, AdsListComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, AdsListPageComponent, AdsListComponent, HeaderComponent, UserPageComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,27 @@ import { AdsListComponent } from './ads-list/ads-list.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AdsListPageComponent } from './ads-list-page/ads-list-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { DummyComponent } from './dummy/dummy.component';
 
 const routes: Routes = [
     { path: 'home', pathMatch: 'full', component: HomeComponent },
     { path: 'ads', pathMatch: 'full', component: AdsListPageComponent,
       // canActivate: [authGuard]
     },
+    { path: 'users/:username',
+      children: [
+        {
+          path: '',
+          component: UserPageComponent
+        },
+        {
+          path: 'bookings',
+          component: DummyComponent
+        }
+      ]
+    },
+
     { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
