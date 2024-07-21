0e36a1862b47921aa8f4447b083fe6a7aa93f67d
toto <antoniolabate19@gmail.com>
2024-07-21 12:36:53 +0200

    now passing ads list to display from parent component

--- src/app/ads-list-page/ads-list-page.component.ts
+++ src/app/ads-list-page/ads-list-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { AdsListComponent } from '../ads-list/ads-list.component';
+import { AdService } from '../services/ads-service.service';
+import { Ad } from '../services/Ad';
 
 @Component({
   selector: 'app-ads-list-page',
@@ -8,4 +10,26 @@ import { AdsListComponent } from '../ads-list/ads-list.component';
 })
 export class AdsListPageComponent {
 
+  adsSignal! : WritableSignal<Ad[]>;
+  ads!: Ad[];
+  
+  constructor(private adService: AdService) {}
+
+  ngOnInit() {
+    this.ads = []
+    this.update()
+  }
+  
+  reload() {
+    this.update()
+  }
+  
+  update() {
+    this.adsSignal = this.adService.getAdsSignal();
+    const searchData = this.adService.getSearchData();
+    
+    this.adService.searchAds(searchData).subscribe((ads) => {
+      this.ads = ads;
+    });
+  }
 }
--- src/app/ads-list/ads-list.component.ts
+++ src/app/ads-list/ads-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, Input, signal, WritableSignal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AdService } from '../services/ads-service.service';
 import { Ad } from '../services/Ad';
@@ -9,27 +9,7 @@ import { Ad } from '../services/Ad';
   styleUrl: './ads-list.component.css'
 })
 export class AdsListComponent {
-  adsSignal! : WritableSignal<Ad[]>;
-  ads!: Ad[];
-  
-  constructor(private adService: AdService) {}
-
-  ngOnInit() {
-    this.ads = []
-    this.update()
-  }
-  
-  reload() {
-    this.update()
-  }
-  
-  update() {
-    this.adsSignal = this.adService.getAdsSignal();
-    const searchData = this.adService.getSearchData();
-    
-    this.adService.searchAds(searchData).subscribe((ads) => {
-      this.ads = ads;
-    });
-  }
+  @Input() 
+    ads!: Ad[];
 
 }
